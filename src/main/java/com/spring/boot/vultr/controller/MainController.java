a743081b0039bfc7a7468dc515e6c56eb19cef1d
cchannawebdeveloper <cchanna.y.oeurng@gmail.com>
2023-03-08 21:02:45 +0700

    first commit

---
+++ src/main/java/com/spring/boot/vultr/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.spring.boot.vultr.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	@GetMapping
+	public String home() {
+		return "home test with vultr";
+	}
+	
+	@GetMapping("/test")
+	public String test() {
+		return "test page";
+	}
+
+}
---
+++ src/test/java/com/spring/boot/vultr/SpringbootVultrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.vultr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVultrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
