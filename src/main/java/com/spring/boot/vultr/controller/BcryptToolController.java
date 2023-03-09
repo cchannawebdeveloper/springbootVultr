1fdcf5b58f2930b2da8c9fd2c8c12564b3251d60
cchannawebdeveloper <cchanna.y.oeurng@gmail.com>
2023-03-09 18:45:14 +0700

    update

---
+++ src/main/java/com/spring/boot/vultr/controller/BcryptToolController.java
@@ -0,0 +1,19 @@
+package com.spring.boot.vultr.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.boot.vultr.model.ResultMessage;
+
+@RestController
+@RequestMapping("/api/v1")
+public class BcryptToolController {
+	
+	
+	@GetMapping("/bcrypt_gnerate")
+	public ResultMessage getBcryptCode() {
+		return new ResultMessage("Sucessfully!", "200", "Request Succesfully");
+	}
+
+}
---
+++ src/main/java/com/spring/boot/vultr/model/ResultMessage.java
@@ -0,0 +1,51 @@
+package com.spring.boot.vultr.model;
+
+public class ResultMessage {
+	private String status;
+	private String code;
+	private String message;
+	
+	public ResultMessage() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public ResultMessage(String status, String code) {
+		this.status = status;
+		this.code = code;
+	}
+
+	public ResultMessage(String status, String code, String message) {
+		this.status = status;
+		this.code = code;
+		this.message = message;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	
+	
+
+}
