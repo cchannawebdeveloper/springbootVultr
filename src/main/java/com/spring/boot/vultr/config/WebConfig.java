112027bbcd3e09adaec68afefa4b339925dbdef9
cchannawebdeveloper <cchanna.y.oeurng@gmail.com>
2023-03-12 23:02:49 +0700

    update

--- src/main/java/com/spring/boot/vultr/config/WebConfig.java
+++ src/main/java/com/spring/boot/vultr/config/WebConfig.java
@@ -1,33 +1,37 @@
-//package com.spring.boot.vultr.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//
-//@Configuration
-//@EnableWebMvc
-//public class WebConfig implements WebMvcConfigurer {
+package com.spring.boot.vultr.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")
+	        .allowedMethods("*")
+	        .allowedOrigins("http://143.198.172.49:8080")
+	        .allowedHeaders("*")
+	        .allowCredentials(false)
+	        .maxAge(-1);
+	        
+	    }
+	
+	
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		exposeDirectory("../category-photos", registry);
+//	}
 //	
-////	@Override
-////	public void addCorsMappings(CorsRegistry registry) {
-////	        registry.addMapping("/**");
-////	    }
-//	
-//	
-////	@Override
-////	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-////		exposeDirectory("../category-photos", registry);
-////	}
-////	
-////	public void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
-////		Path path = Paths.get(pathPattern);
-////		String absolutePath = path.toFile().getAbsolutePath();
-////		String logicalPath = pathPattern.replace("../", "") + "/**";
-////		registry.addResourceHandler(logicalPath)
-////		.addResourceLocations("file:" + absolutePath + "/");
-////	}
-////}
-//	
+//	public void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
+//		Path path = Paths.get(pathPattern);
+//		String absolutePath = path.toFile().getAbsolutePath();
+//		String logicalPath = pathPattern.replace("../", "") + "/**";
+//		registry.addResourceHandler(logicalPath)
+//		.addResourceLocations("file:" + absolutePath + "/");
+//	}
 //}
+	
+}
--- src/main/java/com/spring/boot/vultr/controller/BcryptToolController.java
+++ src/main/java/com/spring/boot/vultr/controller/BcryptToolController.java
@@ -10,12 +10,9 @@ import com.spring.boot.vultr.model.ResultMessage;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://143.198.172.49")
+@CrossOrigin(originPatterns = "http://143.198.172.49:8080")
 public class BcryptToolController {
 	
-	//@CrossOrigin(origins = "http://localhost:8080")
-	
-	//@GetMapping("/bcrypt_gnerate")
 	@RequestMapping(value = "/bcrypt_gnerate", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResultMessage getBcryptCode() {
 		return new ResultMessage("Sucessfully!", "200", "Request Succesfully");
